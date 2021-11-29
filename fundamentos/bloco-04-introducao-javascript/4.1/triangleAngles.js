fd965f48c7c1ff7b3925b8b979bf86fddd475693
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-11-29 20:50:59 -0300

    add angulos do triangulo

---
+++ fundamentos/bloco-04-introducao-javascript/4.1/triangleAngles.js
@@ -0,0 +1,27 @@
+// 3 angulos de um triangulo
+
+const angleA = 60;
+const angleB = 60;
+const angleC = 60;
+
+
+if (angleA <= 0) {
+    console.log('error');
+}
+
+else if (angleB <= 0) {
+    console.log('error');
+}
+
+else if (angleC <= 0) {
+    console.log('error');
+}
+
+else if (angleA + angleB + angleC === 180) {
+    console.log('true');
+}
+
+else if (angleA + angleB + angleC != 180) {
+    console.log('false');
+}
+
