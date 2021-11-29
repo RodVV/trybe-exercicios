7de679a5e936d985a0052347401c806268b3b495
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-11-29 19:26:41 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-javascript/4.1/script.js
@@ -0,0 +1,24 @@
+const a = 22;
+const b = 23;
+const c = 345;
+
+// cinco operações
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// maior valor entre a e b
+
+if (a > b) {
+    console.log('a é maior que b');
+}
+
+else if (b > a) {
+    console.log('b é maior que a');
+}
+
+else {
+    console.log(('a é igual a b'));
+}
