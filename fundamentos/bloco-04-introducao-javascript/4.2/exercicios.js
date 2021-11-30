fbb88b801d2242a0201cca23947dc64e624cc296
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-11-30 19:09:30 -0300

    exercicio 4

--- fundamentos/bloco-04-introducao-javascript/4.2/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript/4.2/exercicios.js
@@ -14,3 +14,14 @@ console.log(sum);
 
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 console.log(sum / numbers.length);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: 
+//               "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+let media = sum / numbers.length;
+if (media > 20) {
+    console.log('valor maior que 20');
+}
+
+else {
+    console.log('valor menor ou igual a 20');
+}
