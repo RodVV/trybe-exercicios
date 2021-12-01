3ef6201cc1a328553a36c1033d5a3684bb70900a
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-12-01 19:29:04 -0300

    exercicio 4

--- fundamentos/bloco-04-introducao-javascript/dia4-js-objects-functions/objects-for-in.js
+++ fundamentos/bloco-04-introducao-javascript/dia4-js-objects-functions/objects-for-in.js
@@ -5,14 +5,19 @@ let info = {
     recorrente: 'Sim'
   };
 
-//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+//1 Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 console.log('Bem-vinda, ' + info.personagem);
 
-//Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+//2 Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 console.log(info);
 
 
-// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+//3 Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
 for (let key in info) {
     console.log(key);
+}
+
+//4 Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console: 
+for (let key in info) {
+    console.log(info[key]);
 }
