912c3c259c4ea6ff0656a2b508aabd18e2101795
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-12-06 18:43:22 -0300

    cria main e muda sua classe para main-content

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando/script.js
@@ -1,3 +1,8 @@
 let body = document.getElementsByTagName('body');
+
 let h1 = document.createElement('h1');
 document.body.appendChild(h1);
+
+let main = document.createElement('main');
+document.body.appendChild(main);
+main.className = 'main-content';
