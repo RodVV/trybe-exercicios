1a616143b7d5a33479aecad4ad27fac1c49b5cc3
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2021-12-06 18:58:51 -0300

    tentando resolver o section

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando/script.js
@@ -6,3 +6,12 @@ document.body.appendChild(h1);
 let main = document.createElement('main');
 document.body.appendChild(main);
 main.className = 'main-content';
+
+let section = document.createElement('section');
+document.main.appendChild(section);
+section.className = 'center-content';
+
+
+
+let p = document.createElement('p');
+document.section.appendChild(p);
